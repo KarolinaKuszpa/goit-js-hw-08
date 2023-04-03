1882d30ec90b37adb14363bb6c256a81c2b58b1e
KarolinaKuszpa <117316629+KarolinaKuszpa@users.noreply.github.com>
2023-04-03 21:36:12 +0200

    Zadanie pierwsze

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const ul = document.querySelector('.gallery');
 
+galleryItems.forEach(item => {
+  const newLi = document.createElement('li');
+  newLi.classList.add('gallery__item');
+
+  ul.appendChild(newLi);
+  newLi.insertAdjacentHTML(
+    'beforeend',
+    `<a class= "gallery__link" href="${item.original}">
+
+  <img
+  class="gallery__image"
+  src="${item.preview}"
+alt="${item.description}" />
+ </a>`
+  );
+});
+const lightbox = new SimpleLightbox('.gallery li a', {
+  captionData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
 console.log(galleryItems);
