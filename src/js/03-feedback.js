3f5a0fbab26a2cd3a5417fed2ace4bb371dcc9b6
KarolinaKuszpa <117316629+KarolinaKuszpa@users.noreply.github.com>
2023-04-12 20:17:19 +0200

    Zadanie 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
 const localStorageKey = 'feedback-form-state';
 
-// zapisujemy stan formularza w local storage
-const saveStateToLocalStorage = () => {
+const stateToLocalStorage = () => {
   const state = {
     email: emailInput.value,
     message: messageInput.value,
@@ -14,8 +13,7 @@ const saveStateToLocalStorage = () => {
   localStorage.setItem(localStorageKey, JSON.stringify(state));
 };
 
-// odczytujemy stan formularza z local storage
-const loadStateFromLocalStorage = () => {
+const loadLocalStorage = () => {
   const state = JSON.parse(localStorage.getItem(localStorageKey));
   if (state) {
     emailInput.value = state.email;
@@ -23,28 +21,23 @@ const loadStateFromLocalStorage = () => {
   }
 };
 
-// usuwamy stan formularza z local storage
-const clearStateFromLocalStorage = () => {
+const clearFormLocalStorage = () => {
   localStorage.removeItem(localStorageKey);
 };
 
-// aktualizujemy stan formularza w local storage nie częściej niż raz na 500ms
-const throttledSaveStateToLocalStorage = throttle(saveStateToLocalStorage, 500);
+const throttledLocalStorage = throttle(stateToLocalStorage, 500);
 
-// nasłuchujemy zdarzeń input i zapisujemy stan formularza w local storage
-form.addEventListener('input', throttledSaveStateToLocalStorage);
+form.addEventListener('input', throttledLocalStorage);
 
-// po załadowaniu strony odczytujemy stan formularza z local storage
-window.addEventListener('load', loadStateFromLocalStorage);
+window.addEventListener('load', loadLocalStorage);
 
-// po wysłaniu formularza czyścimy stan formularza i local storage
 form.addEventListener('submit', event => {
   event.preventDefault();
   clearStateFromLocalStorage();
   emailInput.value = '';
   messageInput.value = '';
-  console.log('Form submitted:', {
-    email: emailInput.value,
-    message: messageInput.value,
-  });
+  {
+    email: emailInput.value;
+    message: messageInput.value;
+  }
 });
